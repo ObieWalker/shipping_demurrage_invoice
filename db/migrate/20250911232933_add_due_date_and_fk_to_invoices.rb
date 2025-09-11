462a04d71312acd89c5e539ce4c052c4f90b1712
obi <obinnawalker@gmail.com>
2025-09-12 00:32:18 +0100

    create migration for due date and foreign key

---
+++ db/migrate/20250911232933_add_due_date_and_fk_to_invoices.rb
@@ -0,0 +1,20 @@
+class AddDueDateAndFkToInvoices < ActiveRecord::Migration[7.2]
+  def change
+    execute <<-SQL
+      ALTER TABLE bl 
+      MODIFY numero_bl VARCHAR(9) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
+    SQL
+
+    execute <<-SQL
+      ALTER TABLE facture 
+      MODIFY numero_bl VARCHAR(9) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
+    SQL
+
+    unless foreign_key_exists?(:facture, :bl, column: :numero_bl, name: :fk_facture_bl_numero)
+      add_foreign_key :facture, :bl,
+        column: :numero_bl,
+        primary_key: :numero_bl,
+        name: :fk_facture_bl_numero
+    end
+  end
+end

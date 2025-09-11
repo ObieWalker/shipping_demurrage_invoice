e4a5c862d60af4dd248f958b81da5ddae1be1b5f
obi <obinnawalker@gmail.com>
2025-09-12 00:39:37 +0100

    - create invoice model - create bill of lading model - create customer model - create refund request model

---
+++ app/models/customer.rb
@@ -0,0 +1,10 @@
+class Customer < ApplicationRecord
+  self.table_name = 'client'
+  self.primary_key = 'id_client'
+
+  alias_attribute :name, :nom
+  alias_attribute :code, :code_client
+
+  has_many :bills_of_lading, foreign_key: :id_client
+  has_many :invoices, primary_key: :code_client, foreign_key: :code_client
+end

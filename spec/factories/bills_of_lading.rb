ecbc180c069e559d192664099634d627c9a9ba9d
obi <obinnawalker@gmail.com>
2025-09-12 03:59:49 +0100

    fix failing tests

--- spec/factories/bills_of_lading.rb
+++ spec/factories/bills_of_lading.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :bill_of_lading, class: 'BillOfLading' do
     initialize_with { BillOfLading.new(attributes) }
     id_bl { SecureRandom.random_number(1_000_000) }
-    numero_bl { SecureRandom.alphanumeric(9).upcase }
+    sequence(:numero_bl) { |n| "BL#{n.to_s.rjust(7, "0")}" } 
     arrival_date { 5.days.ago.change(hour: 0) }
     freetime { 5 }
     nbre_20pieds_sec { 1 }
